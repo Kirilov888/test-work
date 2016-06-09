05b627c3089d06e1862619013772a180a002fa8b
Kyrylov <wnetking1@gmail.com>
2016-06-09 23:39:13 +0300

    initial commit

---
+++ site/js/script.js
@@ -0,0 +1,22 @@
+/*Global variables*/
+
+function initEvents() {
+
+    /*Actions on 'DOM ready' event*/
+    $(function() {
+
+    });
+
+    /*Actions on 'Window load' event*/
+    $(window).on("load", function() {
+
+    });
+
+    /*Actions on 'Window resize' event*/
+    $(window).on("resize", function() {
+
+    });
+}
+
+/*Start all functions and actions*/
+initEvents();
